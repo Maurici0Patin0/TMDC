a884712b65ff0f9364fa73a4e3bc89b4705b10fd
Mauricio Patiño <patto971120@gmail.com>
2023-02-09 22:44:55 -0500

    primer commit

---
+++ assets/js/data/db.js
@@ -0,0 +1,150 @@
+/* DataBase */
+export const productos = [
+  {
+    id: 1,
+    nombre: 'México Oficial',
+    precio: 24,
+    unidades: 61,
+    imagen: 'assets/img/playeras/Mex.png',
+    categoria: 'hoodies'
+  },
+  {
+    id: 2,
+    nombre: 'Argentina Oficial',
+    precio: 22,
+    unidades: 51,
+    imagen: 'assets/img/playeras/arg.png',
+    categoria: 'hoodies'
+  },
+  {
+    id: 3,
+    nombre: 'Brasil Oficial',
+    precio: 24,
+    unidades: 25,
+    imagen: 'assets/img/playeras/Bra.png',
+    categoria: 'hoodies'
+  },
+
+  {
+    id: 4,
+    nombre: 'Ecuador Oficial',
+    precio: 21,
+    unidades: 47,
+    imagen: 'assets/img/playeras/Ecuador_Home.png',
+    categoria: 'hoodies'
+  },
+
+  {
+    id: 5,
+    nombre: 'Alemania Oficial',
+    precio: 23,
+    unidades: 32,
+    imagen: 'assets/img/playeras/Alemania_Home.png',
+    categoria: 'hoodies'
+  },
+  {
+    id: 6,
+    nombre: 'España Oficial',
+    precio: 20,
+    unidades: 62,
+    imagen: 'assets/img/playeras/España.png',
+    categoria: 'hoodies'
+  },
+  {
+    id: 7,
+    nombre: 'Japon Oficial',
+    precio: 19,
+    unidades: 47,
+    imagen: 'assets/img/playeras/Japon_Home.png',
+    categoria: 'hoodies'
+  },
+  {
+    id: 8,
+    nombre: 'Serbia Oficial',
+    precio: 17,
+    unidades: 32,
+    imagen: 'assets/img/playeras/Serbia_Home.png',
+    categoria: 'hoodies'
+  },
+  {
+    id: 9,
+    nombre: 'Suiza Oficial',
+    precio: 21,
+    unidades: 42,
+    imagen: 'assets/img/playeras/suiza.png',
+    categoria: 'hoodies'
+  },
+  {
+    id: 10,
+    nombre: 'México Alternativa',
+    precio: 24,
+    unidades: 55,
+    imagen: 'assets/img/playeras/Mexico_Away.png',
+    categoria: 'sweatshirts'
+  },
+  {
+    id: 11,
+    nombre: 'Argentina Alternativa',
+    precio: 23,
+    unidades: 75,
+    imagen: 'assets/img/playeras/Argentina_Away.png',
+    categoria: 'sweatshirts'
+  },
+  {
+    id: 12,
+    nombre: 'Brasil Alternativa',
+    precio: 24,
+    unidades: 52,
+    imagen: 'assets/img/playeras/Bra_2.png',
+    categoria: 'sweatshirts'
+  },
+  {
+    id: 13,
+    nombre: 'Ecuador Alternativa',
+    precio: 28,
+    unidades: 15,
+    imagen: 'assets/img/playeras/Ecuador_Away.png',
+    categoria: 'sweatshirts'
+  },
+  {
+    id: 14,
+    nombre: 'Alemania Alternativa',
+    precio: 26,
+    unidades: 28,
+    imagen: 'assets/img/playeras/Alemania_Away.png',
+    categoria: 'sweatshirts'
+  },
+  {
+    id: 15,
+    nombre: 'España Alternativa',
+    precio: 23,
+    unidades: 19,
+    imagen: 'assets/img/playeras/España_Away.png',
+    categoria: 'sweatshirts'
+  },
+  {
+    id: 16,
+    nombre: 'Japon Alternativa',
+    precio: 22,
+    unidades: 12,
+    imagen: 'assets/img/playeras/Japon_Away.png',
+    categoria: 'sweatshirts'
+  },
+  {
+    id: 17,
+    nombre: 'Serbia Alternativa',
+    precio: 26,
+    unidades: 18,
+    imagen: 'assets/img/playeras/Serbia_Away.png',
+    categoria: 'sweatshirts'
+  },
+  {
+    id: 18,
+    nombre: 'Suiza Alternativa',
+    precio: 22,
+    unidades: 14,
+    imagen: 'assets/img/playeras/suiza-2.png',
+    categoria: 'sweatshirts'
+  },
+
+]
